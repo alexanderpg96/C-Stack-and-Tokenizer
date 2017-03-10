0bbc8780e53d241df5121af7691af623a68d4aec
Alexander Pearson-Goulart <pearsongoulart@gmail.com>
2017-03-09 19:09:51 -0800

    prog2_3 update

--- prog2_3.cpp
+++ prog2_3.cpp
@@ -7,22 +7,16 @@
 
 using namespace std;
 
-bool Tokenizer::isInt(string s);
-int toInt(string s);
-void simplify();
-bool checkOutput(int c);
-string* Tokenizer::GetTokens();
+int toInteger(string s);
+bool isInt(string s);
+string* GetTokens();
 
-const int CONSTRAINT = 65;
-string * tokens;
+string * t;
 string pushTok[3];
 
 int main(int argc, char **argv) {
 
-    bool check;
     Stack s;
-
-    int count;
 
     cout << "Assignment #2-3, Alexander Pearson-Goulart, pearsongoulart@gmail.com\n";
 
@@ -46,50 +40,22 @@ int main(int argc, char **argv) {
 
         cout << "> ";
 
-        tokens = Tokenizer::GetTokens();
+        t = GetTokens();
 
         //Put in Tokenizer
-        while((tokens[0].length() + tokens[1].length()) > CONSTRAINT) {
-            cout << "ERROR! Input string too long.\n> ";
-            tokens = Tokenizer::GetTokens();
-        }
-
-        simplify();
-
-        if(tokens[1].length() == 0) {
-            count = 1;
-        }
-        else {
-            count = 2;
-        }
-
-        check = checkOutput(count);
-
-        while(!check) {
-            tokens = Tokenizer::GetTokens();
-            if(pushTok[1].length() == 0) {
-                count = 1;
-            }
-            else {
-                count = 2;
-            }
-
-            simplify();
-            check = checkOutput(count);
-
-            if(argCount == numArgs) {
-                exit(0);
-            }
-        }
+        //while((to[0].length() + tokens[1].length()) > CONSTRAINT) {
+          //  cout << "ERROR! Input string too long.\n> ";
+            //tokens = Tokenizer::GetTokens();
+        //}
 
         if(pushTok[1].length() == 0) {
-            if(tokens[0].compare("pop") == 0) {          
+            if(t[0].compare("pop") == 0) {          
                 s.Pop();
             }
         }
         else {
-            if((tokens[0].compare("push") == 0) && isInt(tokens[1])) {
-                s.Push(toInt(tokens[1]));
+            if((t[0].compare("push") == 0) && isInt(t[1])) {
+                s.Push(toInteger(t[1]));
             }
         }
 
@@ -100,50 +66,7 @@ int main(int argc, char **argv) {
     s.Print();
 }
 
-void simplify() {
-    int index = 0;
-    int c = 2;
-
-    while(index < c) {
-        if(Tokenizer::isInt(tokens[index])) {
-           pushTok[index] = "INT ";
-           index++; 
-        }
-        else if(tokens[index].length() == 0) {
-            pushTok[index] = "";
-        }
-        else {
-            pushTok[index] = "STR ";
-            index++;
-        }
-    }
-}
-
-bool checkOutput(int c) {
-    if(c == 2) {
-        if((pushTok[0].compare("STR ") == 0) && (pushTok[1].compare("INT ") == 0)) {
-            return true;
-        }
-        else {
-            cout << "ERROR! Expected STR INT.\n> ";
-            tokens[1] = "";
-            return false;
-        }
-    }
-    else if(c == 1) {
-        if(pushTok[0].compare("STR ") == 0) {
-            pushTok[1] = "";
-            return true;
-        }
-        else {
-            cout << "ERROR! Expected STR.\n> ";
-            pushTok[1] = "";
-            return false;
-        }
-    }
-}
-
-int toInt(string s) {
+int toInteger(string s) {
     int numb;
     istringstream ( s ) >> numb;
     return numb;
